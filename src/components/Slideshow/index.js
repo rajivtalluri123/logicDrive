38028a86ca74477e75bfe9e255d35dc8a2f1adfc
rajiv.talluri <rajiv.talluri@xperi.com>
2022-09-06 19:05:16 -0400

    Add changes for drive logic

--- src/components/Slideshow/index.js
+++ src/components/Slideshow/index.js
@@ -2,12 +2,23 @@ import React from "react";
 import "./style.css";
 import { slideshowText } from "./data";
 import home_img_right from "../../assets/Images/Home-page-right.jpg";
+import drivelogic from "../../assets/Images/driveLogic.png";
 
 function Slideshow({ subheadering1, heading, subheading }) {
   return (
     <section className="home">
       <div className="container">
         <div className="row">
+
+
+         {/*   <div className="header-section mt-2 pt-2">
+              <img
+                  src={drivelogic}
+                  className="img-fluid mt-2 mb-2 home_img_right"
+                  alt="img"
+              />
+            </div>*/}
+
           <div className="col-lg-6 col-md-6 col-12">
             <div className="slideshow-text">
               <h6>{slideshowText.subheadering1}</h6>
