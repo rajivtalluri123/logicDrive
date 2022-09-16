3718ebfe1d69923031844f9149122fb96d41dacb
rajiv.talluri <rajiv.talluri@xperi.com>
2022-09-16 10:30:18 -0400

    comment aboutus button

--- src/components/Slideshow/data.js
+++ src/components/Slideshow/data.js
@@ -1,5 +1,5 @@
 export const slideshowText ={
     subheadering1: '',
     heading:'Drive Logic IT Solutions',
-    subheading:'We accelerate business transformation for our customers. We bring real-world expertise to solve complex technology, business and talent challenges. Our business is supporting business productivity, from matching the right people with great jobs to custom project solutions and consulting. We use innovative technologies and a passion for people to provide you with the best experience possible'
+    subheading:'We accelerate business transformation for our customers. We bring real-world expertise to solve complex technology, business and talent challenges. Our business is supporting business productivity, from matching the right people with great jobs to custom project solutions and consulting. We use innovative technologies and a passion for people to provide you with the best experience possible.'
 }
--- src/components/Slideshow/index.js
+++ src/components/Slideshow/index.js
@@ -24,7 +24,8 @@ function Slideshow({ subheadering1, heading, subheading }) {
               <h6>{slideshowText.subheadering1}</h6>
               <h3>{slideshowText.heading}</h3>
               <p>{slideshowText.subheading}</p>
-              <button className="btn about-btn mt-5 mb-5">About Us</button>
+              {/*couldnt route to about us*/}
+                {/*<button className="btn about-btn mt-5 mb-5">About Us</button>*/}
             </div>
           </div>
           <div className="col-lg-6 col-md-6 col-12">
